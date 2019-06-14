3833b1974e5ff71cbb301ccd9892204aaadcbf55
Jason Stoltzfus <jastoltz24@gmail.com>
2019-06-14 17:03:58 -0400

    Updated version to stack version

--- swiftype_enterprise/__version__.py
+++ swiftype_enterprise/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'swiftype_enterprise'
 __description__ = 'An API client for Swiftype Enterprise'
 __url__ = 'https://github.com/swiftype/swiftype-enterprise-python'
-__version__ = '1.0.0'
+__version__ = '7.2.0-beta.2"'
 __author__ = 'Swiftype'
 __author_email__ = 'eng@swiftype.com'
